961887a6fc20ef5244558e5ef2e21ec6fda9f713
raksinkh <31123749+raksinkh@users.noreply.github.com>
2018-01-21 23:54:03 +0700

    Add files via upload

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,16 +1,96 @@
+import java.util.*;
 
 public class Homework1 {
 
-	public static void main(String[] args) {
-		// Begin of arguments input sample
-		if (args.length > 0) {
-			String input = args[0];
-			if (input.equalsIgnoreCase("251-*32*+")) {
-				System.out.println("(2*(5-1))+(3*2)=14");
-			}
-		}
-		// End of arguments input sample
-		
-		// TODO: Implement your project here
-	}
+    static int i = -1;
+    static String[] ary;
+    public static Stack<Character> mystack = new Stack<Character>();
+    public static Node mtree;
+
+    public static void main(String[] args) {
+        String s = args[0];
+        for (int i = 0; i < s.length(); i++) {
+            mystack.add(s.charAt(i));
+        }
+        mtree = new Node(mystack.pop());
+        infix(mtree);
+        inorder(mtree);
+        System.out.println("=" + calculate(mtree));
+    }
+
+
+    public static class Node {
+        Character key;
+        Node Left = null;
+        Node Right = null;
+
+        public Node() {
+        }
+
+        public Node(Character key) {
+            this.key = key;
+        }
+    }
+
+    public static void inorder(Node n) {
+        if (n.key == '*' || n.key == '/' || n.key == '+' || n.key == '-') {
+            if (n != mtree) {
+                System.out.println("(");
+            }
+            inorder(n.Left);
+            System.out.println(n.key);
+
+            inorder(n.Right);
+
+            if (n != mtree) {
+                System.out.println(")");
+            } else {
+                if ((n != mtree)) {
+                    System.out.println(n.key);
+                }
+            }
+        }
+    }
+
+    public static void infix(Node n) {
+        if (n.key == '*' || n.key == '/' || n.key == '+' || n.key == '-') {   //Check Operator
+            //pop 2 top stacks
+            n.Right = new Node(mystack.pop());
+            infix(n.Right);
+            n.Left = new Node(mystack.pop());
+            infix(n.Left);
+
+        }
+    }
+
+    public static boolean IsOperator(Character a) {
+        switch (a) {
+            case '+':
+                return true;
+            case '-':
+                return true;
+            case '*':
+                return true;
+            case '/':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public static int calculate(Node n) {
+        if(IsOperator(n.key)){
+            switch(n.key){
+                case '+':return calculate(n.Left) + calculate(n.Right);
+                case '-':return calculate(n.Left) - calculate(n.Right);
+                case '*':return calculate(n.Left) * calculate(n.Right);
+                case '/':return calculate(n.Left) / calculate(n.Right);
+                default : System.out.println("Not Operator || Number");return 0;
+            }
+        }
+        else{
+            return Integer.parseInt(n.key.toString());
+        }
+    }
 }
+
